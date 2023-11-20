6e3c028299ff04b2ed0e1f6c5c9b880f8a77c9a3
Thanh Trung <trandacthanhtrungvd@gmail.com>
2023-11-20 16:32:04 +0700

    Final version... perhaps.

--- server/discover.py
+++ server/discover.py
@@ -1,20 +1,28 @@
 import sys
 
-# Syntax check
-if len(sys.argv) != 2:
-    print("Usage: python discover.py hostname")
+def main():
+    # Syntax check
+    if len(sys.argv) != 2:
+        print("Usage: python discover.py hostname")
+        
+    # Get hostname (address) from argument
+    hostname = sys.argv[1]
 
-# Get hostname (address) from argument
-hostname = sys.argv[1]
+    # Call discover function
+    discover(hostname)
 
 # List published filenames from host named hostname
 def discover(hostname):
+    files = []
     with open("peers/" + hostname, "r") as f:
         while True:
             data = f.readline()
             if len(data) == 0:
                 break
-            print(data.split('|')[1].strip('\n'))
+            files.append(data.split('|')[1])
+    data = "".join(files)
+    print(data)
+    return data
 
-# Call discover function
-discover(hostname)
+if __name__ == '__main__':
+    main()
--- test.py
+++
@@ -1,4 +0,0 @@
-try:
-	a = 4 / 0
-except:
-    print("Divided by 0")
